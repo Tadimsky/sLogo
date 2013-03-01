77f97cd33a58c7f5ee465d0711c3684dbb1f6e8e
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-03-01 18:54:26 -0500

    Made the text take multiple commands properly

--- src/view/components/InputField.java
+++ src/view/components/InputField.java
@@ -7,24 +7,26 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import javax.swing.JTextField;
 
+
 /**
  * Text field that holds inputs
+ * 
  * @author Henrique Moraes
- *
+ * 
  */
 public class InputField extends JTextField implements FocusListener {
 
     private static final String HINT = "Command";
-    private static final Font HINT_FONT = new Font("Sans-Serif",Font.ITALIC,14);
+    private static final Font HINT_FONT = new Font("Sans-Serif", Font.ITALIC, 14);
     private static final Font REGULAR_FONT = new Font("Helvetica", Font.PLAIN, 14);
 
-    public InputField(final int fieldSize) {
+    public InputField (final int fieldSize) {
         super(HINT, fieldSize);
         super.setFont(HINT_FONT);
         super.addFocusListener(this);
         // Get the command after press enter
         addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed (ActionEvent e) {
                 String command = getText();
                 // test
                 System.out.println(command);
@@ -33,31 +35,34 @@ public class InputField extends JTextField implements FocusListener {
     }
 
     @Override
-    public void focusGained(FocusEvent e) {
+    public void focusGained (FocusEvent e) {
         super.setFont(REGULAR_FONT);
-        if(this.getText().isEmpty()) {
+        if (this.getText().isEmpty()) {
             super.setText("");
         }
     }
+
     @Override
-    public void focusLost(FocusEvent e) {
-        if(this.getText().isEmpty()) {
-            super.setFont(HINT_FONT);
-            super.setText(HINT);
-        }
-    }
-    
-    @Override
-    public void setText(String text) {
-        super.setText(text);
-        if(text.isEmpty()) {
+    public void focusLost (FocusEvent e) {
+        if (this.getText().isEmpty()) {
             super.setFont(HINT_FONT);
             super.setText(HINT);
         }
     }
 
     @Override
-    public String getText() {
+    public void setText (String text) {
+        super.setText(text);
+        if (text.isEmpty()) {
+            if (!this.hasFocus()) {
+                super.setFont(HINT_FONT);
+                super.setText(HINT);
+            }
+        }
+    }
+
+    @Override
+    public String getText () {
         String typed = super.getText();
         return typed.equals(HINT) ? "" : typed;
     }
