31ae703296b8c67406b991504a8bd1325c01e396
Ziqiang Huang <zh28@duke.edu>
2013-03-25 23:22:17 -0400

    get back CustomCommandwindow working

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -125,6 +125,8 @@ public class Controller {
     }
 
     /**
+     * This method is set private so the Window does not have access to it
+     * 
      * @return the current workspace selected on the tab used internally
      *         so this class can handle its functions
      */
